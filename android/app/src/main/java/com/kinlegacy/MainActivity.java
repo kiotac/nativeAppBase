4493e8c648a66253b69176b0d0da4f92bbceadeb
Mohan <mohans@teknuance.com>
2021-04-11 06:29:37 +0530

    upgrade

--- android/app/src/main/java/com/kinlegacy/MainActivity.java
+++ android/app/src/main/java/com/kinlegacy/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nativeappbase;
+package com.kinlegacy;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "nativeAppBase";
+    return "kinlegacy";
   }
 }
